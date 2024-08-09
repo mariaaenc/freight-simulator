e9191fe08bd47458224bb9d2f91489987d840952
mariaaenc <mariaae.correia@gmail.com>
2024-08-09 14:14:28 -0300

    chore: add @nestjs/swagger to generate docs

--- api-freight-simulator/src/freight-simulation/freight-simulation.controller.ts
+++ api-freight-simulator/src/freight-simulation/freight-simulation.controller.ts
@@ -1,3 +1,4 @@
+import { ApiTags } from '@nestjs/swagger';
 import { Controller, Post, Body } from '@nestjs/common';
 import { FreightSimulationService } from './freight-simulation.service';
 import { CreateFreightSimulationDto } from './dto/create-freight-simulation.dto';
@@ -9,6 +10,7 @@ export class FreightSimulationController {
   ) {}
 
   @Post()
+  @ApiTags('Freight Simulation')
   create(@Body() createFreightSimulationDto: CreateFreightSimulationDto) {
     return this.freightSimulationService.create(createFreightSimulationDto);
   }
