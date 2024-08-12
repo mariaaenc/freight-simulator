a0dc09071948feda3d4df8ad17b4c5cde653525c
mariaaenc <mariaae.correia@gmail.com>
2024-08-12 15:53:54 -0300

    test: add tests to geocoding services

---
+++ api-freight-simulator/src/geocoding/geocoding.api.service.spec.ts
@@ -0,0 +1,84 @@
+import { of, throwError } from 'rxjs';
+import { AxiosResponse } from 'axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { Test, TestingModule } from '@nestjs/testing';
+import { GeocodingApiService } from './geocoding.api.service';
+import { mockGeocodingResponse } from './mocks/geocoding.mock';
+
+const defaultAxiosResponse: AxiosResponse = {
+  data: {},
+  status: 200,
+  statusText: 'OK',
+  headers: {},
+  config: {} as AxiosResponse['config'],
+};
+
+describe('GeocodingApiService', () => {
+  let service: GeocodingApiService;
+  const httpServiceGetFunctionMock = jest.fn();
+  const consoleErrorSpy = jest.spyOn(console, 'error');
+
+  beforeEach(async () => {
+    process.env.GOOGLE_GEOCODE_API_KEY = 'fake_api_key';
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
+      providers: [
+        GeocodingApiService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: httpServiceGetFunctionMock,
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<GeocodingApiService>(GeocodingApiService);
+  });
+
+  describe('getGeocoding', () => {
+    it('should return correct response when api return success', async () => {
+      httpServiceGetFunctionMock.mockReturnValueOnce(
+        of({
+          ...defaultAxiosResponse,
+          data: mockGeocodingResponse,
+        }),
+      );
+      const response = await service.getGeocoding('89222520');
+      expect(response).toEqual(mockGeocodingResponse);
+      expect(httpServiceGetFunctionMock).toHaveBeenCalledWith(
+        'https://maps.googleapis.com/maps/api/geocode/json?address=89222520&key=fake_api_key',
+      );
+      expect(consoleErrorSpy).not.toHaveBeenCalled();
+    });
+
+    it('should throw error when not found geocoding by address', async () => {
+      httpServiceGetFunctionMock.mockReturnValueOnce(
+        of({
+          ...defaultAxiosResponse,
+          data: {
+            results: [],
+          },
+        }),
+      );
+      await expect(service.getGeocoding('89222520')).rejects.toThrow(
+        'Error to get geocoding data',
+      );
+      expect(consoleErrorSpy).toHaveBeenCalledWith(
+        'Failed to get geocoding data from address: 89222520. Error: Not found geocoding information for address: 89222520.',
+      );
+    });
+
+    it('should throw error when something failed', async () => {
+      httpServiceGetFunctionMock.mockReturnValueOnce(
+        throwError(() => new Error('error')),
+      );
+      await expect(service.getGeocoding('89222520')).rejects.toThrow(
+        'Error to get geocoding data',
+      );
+      expect(consoleErrorSpy).toHaveBeenCalledWith(
+        'Failed to get geocoding data from address: 89222520. Error: error',
+      );
+    });
+  });
+});
---
+++ api-freight-simulator/src/geocoding/geocoding.service.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GeocodingService } from './geocoding.service';
+import { mockGeocodingResponse } from './mocks/geocoding.mock';
+import { GeocodingApiService } from './geocoding.api.service';
+
+describe('GeocodingService', () => {
+  let service: GeocodingService;
+  const getGeocodingMock = jest.fn();
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        GeocodingService,
+        {
+          provide: GeocodingApiService,
+          useValue: {
+            getGeocoding: getGeocodingMock,
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<GeocodingService>(GeocodingService);
+  });
+
+  describe('getLongitudeAndLatitude()', () => {
+    it('should return correct value', async () => {
+      getGeocodingMock.mockReturnValueOnce(mockGeocodingResponse);
+      const response = await service.getLongitudeAndLatitude('89222520');
+      expect(response).toEqual({
+        latitude: -26.2688194,
+        longitude: -48.84945159999999,
+      });
+    });
+  });
+});
