fbe3c1badda7185cf5e25090f5c5cfbfabc326e5
mariaaenc <mariaae.correia@gmail.com>
2024-08-17 16:09:38 -0300

    feat: generate operator resource

--- api-freight-simulator/src/app.module.ts
+++ api-freight-simulator/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoModule } from '@/common/mongodb/MongoModule';
 import { FreightSimulationModule } from '@/freight-simulation/freight-simulation.module';
+import { OperatorModule } from '@/operator/operator.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { FreightSimulationModule } from '@/freight-simulation/freight-simulation
     }),
     FreightSimulationModule,
     MongoModule,
+    OperatorModule,
   ],
   controllers: [AppController],
   providers: [AppService],
